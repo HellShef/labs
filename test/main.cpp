2437ae33658cc82cf294528915abd6aca7481370
Apple <apple@macbook-pro-apple-2.local>
2016-07-01 23:14:04 +0300

    Initial Commit

---
+++ test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by Apple on 01.07.16.
+//  Copyright © 2016 Apple. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
